f6b335fc93421f3d7dbb9388cd127c3d81493a1c
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-09-11 10:47:56 -0300

    Update Banner/ Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { BannerComponent } from './shared/banner/banner.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from './shared/container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import {MatCardModule} from '@angular/material/card';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+
 
 
 
@@ -19,14 +25,19 @@ import { MatButtonModule } from '@angular/material/button';
     HeaderComponent,
     FooterComponent,
     CardComponent,
-    BannerComponent
+    BannerComponent,
+    ContainerComponent,
+    HomeComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

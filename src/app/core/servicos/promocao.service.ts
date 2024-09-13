d9ac7b3a673c9d96114c71c4d23f56c30c20f46b
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-09-13 11:15:55 -0300

    Update API Itens Array

--- src/app/core/servicos/promocao.service.ts
+++ src/app/core/servicos/promocao.service.ts
@@ -1,11 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Promocao } from '../types/type';
+import { environment } from 'src/environments/environment';
+
+
+// O Singleton é um design pattern que visa garantir que apenas uma única instância de uma determinada classe exista em todo o sistema. Ele é útil quando é necessário controlar e restringir o número de instâncias de uma classe específica, garantindo que todos os componentes que dependem dela acessem a mesma instância compartilhada. E para configurar um serviço para ser um Singleton, pode fornecer no nível raiz da hierarquia de injeção de dependência ou usando o modificador providedIn: 'root'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromocaoService {
 
+  private apiUrl: string = environment.apiUrl;
+
   constructor(
-    private httpClient: HttpClient
-  ) { }
+          private httpClient: HttpClient
+      ) { }
+
+      listar() : Observable<Promocao[]>{
+          return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocoes`)
+      }
 }
